d13b2b1e446ee6e284b93cc8b32e3abf381840c3
Franlly Mendoza Fañas <77371201+FranllyMendoza@users.noreply.github.com>
2023-03-18 22:44:31 -0300

    .

--- registro/Escritorio_reproductor.Designer.cs
+++ registro/Escritorio_reproductor.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.dataGridView_lista_reproduccion = new System.Windows.Forms.DataGridView();
             this.dataGridView_Canciones = new System.Windows.Forms.DataGridView();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnPLay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_lista_reproduccion)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Canciones)).BeginInit();
             this.SuspendLayout();
@@ -54,15 +59,66 @@
             this.dataGridView_Canciones.Size = new System.Drawing.Size(475, 537);
             this.dataGridView_Canciones.TabIndex = 1;
             // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(176, 613);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(142, 51);
+            this.btnAnterior.TabIndex = 2;
+            this.btnAnterior.Text = "Atras";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnPLay
+            // 
+            this.btnPLay.Location = new System.Drawing.Point(324, 613);
+            this.btnPLay.Name = "btnPLay";
+            this.btnPLay.Size = new System.Drawing.Size(142, 51);
+            this.btnPLay.TabIndex = 3;
+            this.btnPLay.Text = "Play";
+            this.btnPLay.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(472, 613);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(142, 51);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(620, 613);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(142, 51);
+            this.btnSiguiente.TabIndex = 5;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(817, 613);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(142, 51);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
             // Escritorio_reproductor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1181, 689);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPLay);
+            this.Controls.Add(this.btnAnterior);
             this.Controls.Add(this.dataGridView_Canciones);
             this.Controls.Add(this.dataGridView_lista_reproduccion);
             this.Name = "Escritorio_reproductor";
             this.Text = "Escritorio_reproductor";
+            this.Load += new System.EventHandler(this.Escritorio_reproductor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_lista_reproduccion)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Canciones)).EndInit();
             this.ResumeLayout(false);
@@ -73,5 +129,10 @@
 
         private System.Windows.Forms.DataGridView dataGridView_lista_reproduccion;
         private System.Windows.Forms.DataGridView dataGridView_Canciones;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnPLay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnStop;
     }
 }
--- registro/Escritorio_reproductor.cs
+++ registro/Escritorio_reproductor.cs
@@ -7,14 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Escritorio_Rocola.Entidades;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
 
 namespace Escritorio_Rocola.registro
 {
     public partial class Escritorio_reproductor : Form
     {
+        private WindowsMediaPlayer player;
+        public List<Canciones> listaReproduccion;
+        private ObservableCollection<Canciones> _listaReproduccion = new ObservableCollection<Canciones>();
+        private int indiceCancion;
+        private System.Windows.Forms.Timer timer;
+        
+        
         public Escritorio_reproductor()
         {
             InitializeComponent();
+            player = new WindowsMediaPlayer();
+            player.settings.autoStart = true;
+            ActualizarDatos();
         }
+
+        private void Escritorio_reproductor_Load(object sender, EventArgs e)
+        {
+        //    timer.Interval = 5000; // Actualizar cada 5 segundos
+        //    timer.Tick += timer_Tick;
+        //    timer.Start();
+        //    ActualizarDatos(); // Actualiza los datos del DataGridView cuando se carga el formulario
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ActualizarDatos(); // Actualiza los datos del DataGridView cada vez que se llama el temporizador
+        }
+
+        private async void ActualizarDatos()
+        {
+
+            try
+            {
+                // Crear una instancia de HttpClient
+                HttpClient client = new HttpClient();
+
+                // Configurar la URL de la API
+                string apiUrl = "http://localhost:3000/canciones/";
+
+                string jsonData = await client.GetStringAsync(apiUrl); // Obtener datos de la API en formato JSON
+
+                listaReproduccion = JsonConvert.DeserializeObject<List<Canciones>>(jsonData); // Convertir JSON a una lista de objetos DataModel
+                dataGridView_Canciones.DataSource = listaReproduccion;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (listaReproduccion == null || listaReproduccion.Count == 0)
+            {
+                MessageBox.Show("No hay canciones en la lista de reproducción.");
+                return;
+            }
+
+            player.URL = listaReproduccion[0].ruta;
+            player.controls.play();
+
+        }
+
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            player.controls.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
+        }
+
+
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (listaReproduccion == null || listaReproduccion.Count == 0)
+            {
+                MessageBox.Show("No hay canciones en la lista de reproducción.");
+                return;
+            }
+
+            if (indiceCancion > 0)
+            {
+                indiceCancion--;
+            }
+            else
+            {
+                indiceCancion = listaReproduccion.Count - 1;
+            }
+
+            player.URL = listaReproduccion[indiceCancion].ruta;
+            player.controls.play();
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (listaReproduccion == null || listaReproduccion.Count == 0)
+            {
+                MessageBox.Show("No hay canciones en la lista de reproducción.");
+                return;
+            }
+
+            if (indiceCancion < listaReproduccion.Count - 1)
+            {
+                indiceCancion++;
+            }
+            else
+            {
+                indiceCancion = 0;
+            }
+
+            player.URL = listaReproduccion[indiceCancion].ruta;
+            player.controls.play();
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
